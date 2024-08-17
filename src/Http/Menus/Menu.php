4b5730a64bed6c7a37f0ef4935b4553760e961db
Narsilien <rigaux.jonathan@gmail.com>
2024-08-17 11:37:30 +0200

    Add menu visibility

--- src/Http/Menus/Menu.php
+++ src/Http/Menus/Menu.php
@@ -4,6 +4,7 @@ namespace Narsil\Auth\Http\Menus;
 
 #region USE
 
+use Narsil\Menus\Enums\VisibilityEnum;
 use Narsil\Menus\Http\Menus\AbstractMenu;
 use Narsil\Menus\Models\MenuNode;
 
@@ -26,10 +27,12 @@ class Menu extends AbstractMenu
         return [[
             MenuNode::LABEL => 'Users',
             MenuNode::URL => '/backend/users',
+            MenuNode::VISIBILITY => VisibilityEnum::AUTH,
             MenuNode::RELATIONSHIP_ICON => 'lucide/user',
         ], [
             MenuNode::LABEL => 'Login logs',
             MenuNode::URL => '/backend/login-logs',
+            MenuNode::VISIBILITY => VisibilityEnum::AUTH,
             MenuNode::RELATIONSHIP_ICON => 'lucide/log-in',
         ]];
     }
