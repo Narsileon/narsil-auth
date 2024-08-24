2b8d206d4f7dfe31d6c4fa363982406af9d43c9d
Narsilien <rigaux.jonathan@gmail.com>
2024-08-24 21:13:57 +0200

    Export models

--- resources/js/Types.ts
+++ resources/js/Types.ts
@@ -1,3 +1,15 @@
+export type LoginLogModel = {
+	active: boolean;
+	created_at: string;
+	device: string;
+	id: number;
+	ip_addresses: string | string[];
+	session_id: string;
+	updated_at: string;
+	user_id: number;
+	user: UserModel;
+};
+
 export type SessionModel = {
 	device: string;
 	id: string;
@@ -6,4 +18,23 @@ export type SessionModel = {
 	last_activity: string;
 	payload: string;
 	user_agent: string;
+};
+
+export type UserModel = {
+	active: boolean;
+	birth_country: string;
+	birthdate: string;
+	birthplace: string;
+	created_at: string;
+	email_verified_at: string;
+	email: string;
+	first_name: string;
+	id: number;
+	last_name: string;
+	password: string;
+	remember_token: string;
+	two_factor_recovery_codes: string;
+	two_factor_secret: string;
+	updated_at: string;
+	username: string;
 };
