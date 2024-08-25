191a8a8546520379e22c3d54781f06e9d13a6803
Narsilien <rigaux.jonathan@gmail.com>
2024-08-25 14:15:17 +0200

    Remove sheet portal

--- resources/js/Components/UserMenu/UserMenuSheetContent.tsx
+++ resources/js/Components/UserMenu/UserMenuSheetContent.tsx
@@ -10,7 +10,6 @@ import NavigationMenuList from "@narsil-ui/Components/NavigationMenu/NavigationM
 import Separator from "@narsil-ui/Components/Separator/Separator";
 import SheetClose from "@narsil-ui/Components/Sheet/SheetClose";
 import SheetContent, { SheetContentProps } from "@narsil-ui/Components/Sheet/SheetContent";
-import SheetPortal from "@narsil-ui/Components/Sheet/SheetPortal";
 
 interface UserMenuSheetContentProps extends Partial<SheetContentProps> {
 	authenticated?: boolean;
@@ -22,72 +21,70 @@ const UserMenuSheetContent = React.forwardRef<HTMLDivElement, UserMenuSheetConte
 		const { trans } = useTranslationsStore();
 
 		return (
-			<SheetPortal>
-				<SheetContent
-					ref={ref}
-					className={cn("absolute inset-0 w-full", className)}
-					side='left'
-					{...props}
-				>
-					<NavigationMenu orientation='vertical'>
-						<NavigationMenuList>
-							{children}
+			<SheetContent
+				ref={ref}
+				className={cn("absolute inset-0 w-full", className)}
+				side='left'
+				{...props}
+			>
+				<NavigationMenu orientation='vertical'>
+					<NavigationMenuList>
+						{children}
 
-							{authenticated ? (
-								<>
-									{children ? <Separator /> : null}
+						{authenticated ? (
+							<>
+								{children ? <Separator /> : null}
 
+								<SheetClose asChild={true}>
+									<NavigationMenuItem
+										className={navigationMenuTriggerStyle()}
+										asChild={true}
+									>
+										<Link
+											as='button'
+											href={route("logout")}
+											method='post'
+										>
+											<LogOut className='h-5 w-5' />
+											{trans("Sign out")}
+										</Link>
+									</NavigationMenuItem>
+								</SheetClose>
+							</>
+						) : (
+							<>
+								{children ? <Separator /> : null}
+
+								<SheetClose asChild={true}>
+									<NavigationMenuItem
+										className={navigationMenuTriggerStyle()}
+										asChild={true}
+									>
+										<Link href={route("login")}>
+											<LogIn className='h-5 w-5' />
+											{trans("Sign in")}
+										</Link>
+									</NavigationMenuItem>
+								</SheetClose>
+
+								{registerable ? (
 									<SheetClose asChild={true}>
 										<NavigationMenuItem
 											className={navigationMenuTriggerStyle()}
 											asChild={true}
 										>
-											<Link
-												as='button'
-												href={route("logout")}
-												method='post'
-											>
-												<LogOut className='h-5 w-5' />
-												{trans("Sign out")}
+											<Link href={route("register")}>
+												<UserPlus className='h-5 w-5' />
+												{trans("Register")}
 											</Link>
 										</NavigationMenuItem>
 									</SheetClose>
-								</>
-							) : (
-								<>
-									{children ? <Separator /> : null}
-
-									<SheetClose asChild={true}>
-										<NavigationMenuItem
-											className={navigationMenuTriggerStyle()}
-											asChild={true}
-										>
-											<Link href={route("login")}>
-												<LogIn className='h-5 w-5' />
-												{trans("Sign in")}
-											</Link>
-										</NavigationMenuItem>
-									</SheetClose>
-
-									{registerable ? (
-										<SheetClose asChild={true}>
-											<NavigationMenuItem
-												className={navigationMenuTriggerStyle()}
-												asChild={true}
-											>
-												<Link href={route("register")}>
-													<UserPlus className='h-5 w-5' />
-													{trans("Register")}
-												</Link>
-											</NavigationMenuItem>
-										</SheetClose>
-									) : null}
-								</>
-							)}
-						</NavigationMenuList>
-					</NavigationMenu>
-				</SheetContent>
-			</SheetPortal>
+								) : null}
+							</>
+						)}
+					</NavigationMenuList>
+				</NavigationMenu>
+			</SheetContent>
 		);
 	}
 );
